553b103b6f3cb978386744b254aab5ec7650602d
Ahda Filza <afilzag@gmail.com>
2026-01-04 16:36:56 +0700

    AF: done profile section

---
+++ src/lib/config/skills.ts
@@ -0,0 +1,15 @@
+import next from '$lib/assets/next.png';
+import vue from '$lib/assets/vue.png';
+import svelte from '$lib/assets/svelte.png';
+import maui from '$lib/assets/maui.png';
+import jsts from '$lib/assets/jsts.png';
+import go from '$lib/assets/go.png';
+
+export const SKILLS_SECTIONS = [
+	{ src: next, alt: 'Nextjs', text: 'Nextjs' },
+	{ src: vue, alt: 'Vue', text: 'Vue' },
+	{ src: svelte, alt: 'Svelte', text: 'Svelte' },
+	{ src: maui, alt: 'NET MAUI', text: 'NET MAUI' },
+	{ src: jsts, alt: 'JS/TS', text: 'JS/TS' },
+	{ src: go, alt: 'Golang', text: 'Golang' }
+];
---
+++ src/lib/layout/Profile.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Me from '$lib/assets/Me.webp';
+	import { SKILLS_SECTIONS } from '$lib/config/skills';
+</script>
+
+<div class="flex h-full flex-col items-center justify-center gap-10 font-roboto md:flex-row">
+	<div class="w-fit px-10 md:w-2/6 md:px-0">
+		<h1 class="mb-5 text-xl font-extrabold">Hello, <br />My name is Ahda.</h1>
+		<p class="text-sm">Im into Software Engineering.</p>
+		<text class="text-sm"
+			>I usually work with React/Next, Vue, Svelte, and .NET Maui while also quite familiar with
+			Express, Fastify, and Gin & Gorm.
+		</text>
+
+		<div class="mt-5 flex justify-center gap-2 md:mt-2 md:justify-start">
+			{#each SKILLS_SECTIONS as s}
+				<div
+					class="group relative h-10 w-10 rounded-full bg-white transition duration-500 hover:-translate-y-0.5"
+				>
+					<img alt={s.alt} src={s.src} />
+					<span
+						class="absolute -bottom-8 left-1/2 -translate-x-1/2 rounded-full p-1 text-center text-xs whitespace-nowrap opacity-0 transition duration-500 group-hover:opacity-100 dark:bg-[#44444E]"
+						>{s.text}</span
+					>
+				</div>
+			{/each}
+		</div>
+	</div>
+
+	<div class="relative w-fit md:w-2/6">
+		<img
+			alt="Ahda Filza"
+			src={Me}
+			class="relative z-50 h-64 w-64 rounded-2xl grayscale-25 duration-1000 hover:translate-y-1 hover:grayscale-0 md:h-auto md:w-auto"
+		/>
+		<div
+			class="absolute top-32 z-0 h-56 w-64 rounded-2xl bg-gray-300 md:right-0 md:h-80 md:w-80 dark:bg-[#44444E]"
+		></div>
+	</div>
+</div>
