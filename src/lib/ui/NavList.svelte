2e38a52dc62c986ff4cbca2218e6df06d5950d64
Ahda Filza <afilzag@gmail.com>
2026-01-05 13:06:55 +0700

    AF: disable underlining active nav on mobile

--- src/lib/ui/NavList.svelte
+++ src/lib/ui/NavList.svelte
@@ -10,14 +10,16 @@
 {#each NAV_SECTIONS as i, index}
 	<li class="text-gray-400 hover:text-black dark:hover:text-white">
 		<button
-			class="relative cursor-pointer pb-1 data-[active=true]:text-black dark:data-[active=true]:text-white"
+			class="relative cursor-pointer pb-1 md:data-[active=true]:text-black md:dark:data-[active=true]:text-white"
 			data-active={isSectionActive(i.id)}
 			onclick={() => navigate(index)}
 		>
 			{i.label}
 
 			{#if isSectionActive(i.id)}
-				<span class="absolute bottom-0 left-0 h-0.5 w-full bg-black dark:bg-white"></span>
+				<span
+					class="absolute bottom-0 left-0 h-0.5 w-full bg-black opacity-0 md:opacity-100 dark:bg-white"
+				></span>
 			{/if}
 		</button>
 	</li>
