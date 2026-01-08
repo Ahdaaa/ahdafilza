7cc4b9190f78105e202fc2fa168cef1515985620
Ahda Filza <afilzag@gmail.com>
2026-01-08 23:53:38 +0700

    AF: finalize contact section

---
+++ src/lib/layout/Contacts.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ContactIcon from '$lib/ui/ContactIcon.svelte';
+</script>
+
+<div class="m-auto w-4/6 max-w-max font-roboto">
+	<h1 class="mb-5 text-xl font-extrabold">Contacts</h1>
+	<p class="text-base">Feel free to contact me if you want to discuss about something!</p>
+	<div class="mt-5 flex gap-5">
+		<ContactIcon />
+	</div>
+</div>
---
+++ src/lib/ui/Tooltip.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	// slot is deprecated, we use snippet
+	const { label, size, children }: { label: string; size: number; children: Snippet } = $props();
+</script>
+
+<div
+	class={`group relative h-${size} w-${size} rounded-full bg-transparent transition duration-500 hover:-translate-y-0.5`}
+	class:dark:bg-white={label == 'Next'}
+>
+	{@render children()}
+	<span
+		class="absolute -bottom-8 left-1/2 -translate-x-1/2 rounded-full px-2 py-1 text-xs whitespace-nowrap opacity-0 transition duration-500 group-hover:opacity-100 dark:bg-[#44444E]"
+	>
+		{label}
+	</span>
+</div>
